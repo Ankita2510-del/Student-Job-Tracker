9b9695a966af07aa26da879ff4f9277ada7e884b
Ankita2510-del <ay795272@gmail.com>
2026-07-14 21:07:24 +0530

    Create homepage UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav className="navbar">
+        <h2>Student Job Tracker</h2>
+
+        <div>
+          <button>Login</button>
+          <button>Register</button>
+        </div>
+      </nav>
+
+      <section className="hero">
+        <h1>Track Your Dream Job</h1>
+
+        <p>
+          Keep all your internship and job applications in one place.
+        </p>
+
+        <button>Get Started</button>
+      </section>
+    </>
+  );
+}
+
+export default App;
